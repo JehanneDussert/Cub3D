297b02d0c6b5d837cc6d7496702b92f5d018ed15
Jehanne DUSSERT <jdussert@student.42.fr>
2020-04-29 16:39:15 +0200

    Move OK

--- files/srcs/raycasting/ft_move.c
+++ files/srcs/raycasting/ft_move.c
@@ -2,37 +2,33 @@
 
 void move_up(t_all *all)
 {
-  printf("move up\n");
-  if(all->map->map[(int)(all->map->pos_y + all->vec->dirY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
+  if (all->map->map[(int)(all->map->pos_y + all->vec->dirY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
     all->map->pos_y += all->vec->dirY * all->keys->moveSpeed;
-  if(all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x + all->vec->dirX * all->keys->moveSpeed)] != '1') 
+  if (all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x + all->vec->dirX * all->keys->moveSpeed)] != '1') 
     all->map->pos_x += all->vec->dirX * all->keys->moveSpeed;
 }
 
 void move_down(t_all *all)
 {
-  printf("move down\n");
-  if(all->map->map[(int)(all->map->pos_y - all->vec->dirY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
+  if (all->map->map[(int)(all->map->pos_y - all->vec->dirY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
     all->map->pos_y -= all->vec->dirY * all->keys->moveSpeed;
-  if(all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x - all->vec->dirX * all->keys->moveSpeed)] != '1') 
+  if (all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x - all->vec->dirX * all->keys->moveSpeed)] != '1') 
     all->map->pos_x -= all->vec->dirX * all->keys->moveSpeed;
 }
 
 void move_right(t_all *all)
 {
-  printf("move right\n");
-  if(all->map->map[(int)(all->map->pos_y + all->vec->planeY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
+  if (all->map->map[(int)(all->map->pos_y + all->vec->planeY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
     all->map->pos_y += all->vec->planeY * all->keys->moveSpeed;
-  if(all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x + all->vec->planeX * all->keys->moveSpeed)] != '1') 
+  if (all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x + all->vec->planeX * all->keys->moveSpeed)] != '1') 
      all->map->pos_x += all->vec->planeX * all->keys->moveSpeed;
 }
 
 void move_left(t_all *all)
 {
-  printf("move left\n");
-  if(all->map->map[(int)(all->map->pos_y - all->vec->planeY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
+  if (all->map->map[(int)(all->map->pos_y - all->vec->planeY * all->keys->moveSpeed)][(int)(all->map->pos_x)] != '1')
     all->map->pos_y -= all->vec->planeY * all->keys->moveSpeed;
-  if(all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x - all->vec->planeX * all->keys->moveSpeed)] != '1') 
+  if (all->map->map[(int)(all->map->pos_y)][(int)(all->map->pos_x - all->vec->planeX * all->keys->moveSpeed)] != '1') 
      all->map->pos_x -= all->vec->planeX * all->keys->moveSpeed;
 }
 
@@ -49,6 +45,5 @@ int  ft_move(t_all *all)
     move_right(all);
   if ((ft_rotation(all) != 0))
     return (ft_error(9, all));
-  printf("heyyy\n");
   return (0);
 }
