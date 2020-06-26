d4dbb2172201419656b3b0221267cd61593b96cf
Jehanne DUSSERT <jdussert@student.42.fr>
2020-06-26 18:05:08 +0200

    Norme

--- files/srcs/utils/ft_error.c
+++ files/srcs/utils/ft_error.c
@@ -12,43 +12,40 @@
 
 #include "../../includes/cub3d.h"
 
-void    ft_free_all(t_all **all)
+void	ft_file_prbl(int msg)
 {
-    if (*all)
-    {
-        free (*all);
-        *all = NULL;
-    }
+	if (msg == 0)
+		ft_putstr_fd("[ERROR]\nInvalid file.", 0);
+	else if (msg == 5)
+		ft_putstr_fd("[ERROR]\nFile opening problem.", 0);
+	else if (msg == 6)
+		ft_putstr_fd("[ERROR]\nWrong resolution.", 0);
+	else if (msg == 8)
+		ft_putstr_fd("[ERROR]\nWrong texture.", 0);
+	else if (msg == 10)
+		ft_putstr_fd("[ERROR]\nWrong map.", 0);
+	else if (msg == 12)
+		ft_putstr_fd("[ERROR]\nWrong colors.", 0);
 }
 
-int     ft_error(int msg, t_all *all)
+int		ft_error(int msg, t_all *all)
 {
-    if (msg == 0)
-        ft_putstr_fd("[ERROR]\nInvalid file.", 0);
-    else if (msg == 1)
-        ft_putstr_fd("[ERROR]\nWrong argument.", 0);
-    else if (msg == 2)
-        ft_putstr_fd("[ERROR]\nMemory allocation.", 0);
-    else if (msg == 3)
-        ft_putstr_fd("[ERROR]\nParsing error.", 0);
-    else if (msg == 4)
-        ft_putstr_fd("[ERROR]\nRaycasting error.", 0);
-    else if (msg == 5)
-        ft_putstr_fd("[ERROR]\nFile opening problem.", 0);
-    else if (msg == 6)
-        ft_putstr_fd("[ERROR]\nWrong resolution.", 0);
-    else if (msg == 7)
-        ft_putstr_fd("[ERROR]\nMlx failed.", 0);
-    else if (msg == 8)
-        ft_putstr_fd("[ERROR]\nWrong texture.", 0);
-    else if (msg == 9)
-        ft_putstr_fd("[ERROR]\nMovement error.", 0);
-    else if (msg == 10)
-        ft_putstr_fd("[ERROR]\nWrong map.", 0);
-    else if (msg == 11)
-        ft_putstr_fd("[ERROR]\nToo much sprites.", 0);
-    else if (msg == 12)
-        ft_putstr_fd("[ERROR]\nWrong colors.", 0);
-    ft_killwindow(all);
-    return (1);
+	if (msg == 0 || msg == 5 || msg == 6 || msg == 8 || msg == 10 || msg == 12)
+		ft_file_prbl(msg);
+	else if (msg == 1)
+		ft_putstr_fd("[ERROR]\nWrong argument.", 0);
+	else if (msg == 2)
+		ft_putstr_fd("[ERROR]\nMemory allocation.", 0);
+	else if (msg == 3)
+		ft_putstr_fd("[ERROR]\nParsing error.", 0);
+	else if (msg == 4)
+		ft_putstr_fd("[ERROR]\nRaycasting error.", 0);
+	else if (msg == 7)
+		ft_putstr_fd("[ERROR]\nMlx failed.", 0);
+	else if (msg == 9)
+		ft_putstr_fd("[ERROR]\nMovement error.", 0);
+	else if (msg == 11)
+		ft_putstr_fd("[ERROR]\nToo much sprites.", 0);
+	ft_killwindow(all);
+	return (1);
 }
