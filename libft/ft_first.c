124f2f114c5691d9c0b8bcb1974611578003d46c
JehanneDussert <jdussert@student.42.fr>
2020-08-14 14:00:09 +0200

    Fixes textures

--- libft/ft_first.c
+++ libft/ft_first.c
@@ -6,26 +6,29 @@
 /*   By: jdussert <jdussert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 16:34:34 by jdussert          #+#    #+#             */
-/*   Updated: 2020/08/03 15:13:33 by jdussert         ###   ########.fr       */
+/*   Updated: 2020/08/14 11:08:04 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_first(char *haystack, char *needle)
+int	ft_first(char *haystack, char *needle, int *start)
 {
 	int	len_h;
 	int	len_n;
 	int	i;
-
-	i = 0;
+	int	j;
+	
+	i = *start;
+	j = 0;
 	len_h = ft_strlen(haystack);
 	len_n = ft_strlen(needle);
-	while (i < len_n && i < len_h && haystack[i] == needle[i])
+	while (j < len_n && i < len_h && haystack[i] == needle[j])
 	{
-		if (i + 1 == len_n)
+		if (j + 1 == len_n)
 			return (1);
 		i++;
+		j++;
 	}
 	return (0);
 }
